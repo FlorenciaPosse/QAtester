683ddfdd5f35e765ffa0b6543ec9912aea83b395
FlorenciaPosse <florenciaposse93@gmail.com>
2026-05-17 21:42:06 +0200

    Update insert.sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO formflorenciaposse (name, email, subject, age, message)
 VALUES
-('ana', 'ana@gmail.com', 'information', 20, 'hola testing'),
+('ana', 'ana@gmail.com', 'information', NULL, 'hola testing'),
 ('pedro', 'pedro@gmail.com', 'job', 20, 'pedro mensaje'),
 ('maria', 'maria@yahoo.com', 'other', 20, 'mensaje de prueba'),
 ('ana', 'ana.work@gmail.com', 'job', 30, 'test message'),
