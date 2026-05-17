b0c272b6ae3b1d03251d89965744c4e7c4c13a8b
FlorenciaPosse <florenciaposse93@gmail.com>
2026-05-17 21:37:01 +0200

    Update create.sql

---
+++ sql/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuarios(
+id SERIAL PRIMARY KEY,
+name VARCHAR(100),
+email VARCHAR (100),
+subject VARCHAR(100)NOT NULL,
+age INTEGER CHECK (age >= 18)NOT NULL,
+message VARCHAR (500)NOT NULL
+);
