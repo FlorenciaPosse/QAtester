d734c879e780c2ae77022ca9b4384f03da18f3c5
FlorenciaPosse <florenciaposse93@gmail.com>
2026-05-19 19:11:34 +0200

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE formflorenciaposse(
 id SERIAL PRIMARY KEY,
 name VARCHAR(100),
-email VARCHAR (100),
+email VARCHAR (100)NOT NULL,
 subject VARCHAR(100)NOT NULL,
 age INTEGER CHECK (age >= 18)NOT NULL,
 message VARCHAR (500)NOT NULL
