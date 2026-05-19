11571c0b96d79f83638eae2cdc00f51d27063618
FlorenciaPosse <florenciaposse93@gmail.com>
2026-05-19 12:47:27 +0200

    Update fixes.sql
    arreglo de errores de conexión

--- sql/fixes.sql
+++ sql/fixes.sql
@@ -1,1 +1,16 @@
+GRANT INSERT ON public.formflorenciaposse TO anon;
 
+GRANT SELECT ON public.formflorenciaposse TO anon;
+ALTER TABLE formflorenciaposse ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "public insert"
+ON formflorenciaposse
+FOR INSERT
+TO anon
+WITH CHECK (true);
+
+CREATE POLICY "public read"
+ON formflorenciaposse
+FOR SELECT
+TO anon
+USING (true);
